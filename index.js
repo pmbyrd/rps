9993af82c159ef4157f8f8636acdeab0292fca7d
pmbyrd <pmbyrd@gmail.com>
2025-06-02 21:46:32 -0700

    feat: game can be played it is rough but it works

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ function getComputerChoice(array = choices) {
 }
 
 function getHumanChoice() {
-    let choice = prompt("Please enter a choice of rock, paper or scissors")
+    // let choice = prompt("Please enter a choice of rock, paper or scissors")
+    let choice = "rock"
     return choice.toLowerCase()
 }
 
@@ -26,24 +27,48 @@ function playRound(params) {
         console.log(result)
         return result
     } else if (humanChoice === "paper" && computerChoice === "rock") {
-        console.log("Paper covers rock, you win")
+        result = "Paper covers rock, you win"
+        console.log(result)
         humanScore ++
+        return result
     } else if (humanChoice === "rock" && computerChoice === "paper") {
-        console.log("Paper covers rock, you lose")
+        result = "Paper covers rock, you lose"
         computerScore ++
+        return result
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        console.log("Scissors cuts paper, you win!")
+        result = ("Scissors cuts paper, you win!")
+        console.log(result)
         humanScore ++
+        return result
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        console.log("Scissors cuts paper, you lose!")
+        result = ("Scissors cuts paper, you lose!")
+        console.log(result)
         computerScore ++
+        return result
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        console.log("Rock crushes scissors, you win!")
+        result = ("Rock crushes scissors, you win!")
+        console.log(result)
         humanScore ++
+        return result
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        console.log("Rock crushes scissors, you lose!")
+        result = ("Rock crushes scissors, you lose!")
+        console.log(result)
         computerScore ++
+        return result
     }
-    console.log(humanScore, computerScore)
     return [computerScore, humanScore]
 }
+
+function game() {
+    // call the play game
+    // first to five is the winner and the game wins
+    playRound()
+    console.log(humanScore, computerScore)
+    if (humanScore === 5) {
+        alert("Congradulations, you won!!!!!!!!!!!!!!!")        
+    }   else if (computerScore === 5) {
+        alert("You lost, would you like to play again?")
+    }
+}
+
+game()

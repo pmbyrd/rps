366d65d23300f19b551c3419e2f427ab367a6a50
pmbyrd <pmbyrd@gmail.com>
2025-06-02 21:24:42 -0700

    feat: basic game logic

--- index.js
+++ index.js
@@ -12,12 +12,38 @@ function getComputerChoice(array = choices) {
 
 function getHumanChoice() {
     let choice = prompt("Please enter a choice of rock, paper or scissors")
-    return choice
+    return choice.toLowerCase()
 }
 
 function playRound(params) {
-    console.log("inside the play round function")
     let computerChoice = getComputerChoice()
+    let humanChoice = getHumanChoice()
+    let result
     console.log(computerChoice)
+    console.log(humanChoice)
+    if (humanChoice === computerChoice) {
+        result = "It's a draw"
+        console.log(result)
+        return result
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log("Paper covers rock, you win")
+        humanScore ++
+    } else if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log("Paper covers rock, you lose")
+        computerScore ++
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log("Scissors cuts paper, you win!")
+        humanScore ++
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log("Scissors cuts paper, you lose!")
+        computerScore ++
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log("Rock crushes scissors, you win!")
+        humanScore ++
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log("Rock crushes scissors, you lose!")
+        computerScore ++
+    }
+    console.log(humanScore, computerScore)
+    return [computerScore, humanScore]
 }
-

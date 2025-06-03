ea585dac259d171b8ea918d9feed77630538138d
pmbyrd <pmbyrd@gmail.com>
2025-06-02 20:37:13 -0700

    feat: undid html console based game around javascript

--- index.js
+++ index.js
@@ -2,4 +2,22 @@ console.log("hello world")
 
 const choices = ["rock", "paper", "scissors"]
 
-console.log(choices)
+let humanScore = 0
+let computerScore = 0
+
+function getComputerChoice(array = choices) {
+    let number = Math.floor(Math.random() * choices.length)
+    return choices[number]
+}
+
+function getHumanChoice() {
+    let choice = prompt("Please enter a choice of rock, paper or scissors")
+    return choice
+}
+
+function playRound(params) {
+    console.log("inside the play round function")
+    let computerChoice = getComputerChoice()
+    console.log(computerChoice)
+}
+

60f744c64c73cc1fed817b1bb3529b4ec8143124
pmbyrd <pmbyrd@gmail.com>
2025-06-11 19:50:02 -0700

    feat: added functions to handle game logic getting the human choice updating the display

--- index.js
+++ index.js
@@ -1,66 +1,76 @@
 console.log("hello world")
 
 const choicesArray = ["rock", "paper", "scissors"]
-
-const rock = document.querySelector(".rock")
-const paper = document.querySelector(".paper")
-const scissors = document.querySelector(".scissors")
-const choices = document.querySelector(".choices")
 let humanScore = 0
 let computerScore = 0
+const resultDisplay = document.querySelector(".result")
+const choices = document.querySelector(".choices")
 
 function getComputerChoice(array = choicesArray) {
     let number = Math.floor(Math.random() * choicesArray.length)
     return choicesArray[number]
 }
 
-function getHumanChoice(e) {
-    // let choice = prompt("Please enter a choice of rock, paper or scissors")
-    console.log(e.target)
+function getHumanChoice(target) {
+    if (target === "rock") {
+        console.log("rock")
+        return "rock"
+    } else if (target === "paper") {
+        console.log("paper")
+        return "paper"
+    } else if (target === "scissors") {
+        console.log("scissors")
+        return "scissors"
+    }
 }
 
-
-function playRound(params) {
-    let computerChoice = getComputerChoice()
-    let humanChoice = getHumanChoice()
+function updateDisplay (humanChoice, computerChoice) {
     let result
-    console.log(computerChoice)
-    console.log(humanChoice)
+
     if (humanChoice === computerChoice) {
         result = "It's a draw"
         console.log(result)
-        return result
+        resultDisplay.textContent = result
     } else if (humanChoice === "paper" && computerChoice === "rock") {
         result = "Paper covers rock, you win"
         console.log(result)
-        humanScore ++
-        return result
+        humanScore++
+        resultDisplay.textContent = result
     } else if (humanChoice === "rock" && computerChoice === "paper") {
         result = "Paper covers rock, you lose"
-        computerScore ++
-        return result
+        computerScore++
+        resultDisplay.textContent = result
     } else if (humanChoice === "scissors" && computerChoice === "paper") {
         result = ("Scissors cuts paper, you win!")
         console.log(result)
-        humanScore ++
-        return result
+        humanScore++
+        resultDisplay.textContent = result
     } else if (humanChoice === "paper" && computerChoice === "scissors") {
         result = ("Scissors cuts paper, you lose!")
         console.log(result)
-        computerScore ++
-        return result
+        computerScore++
+        resultDisplay.textContent = result
     } else if (humanChoice === "rock" && computerChoice === "scissors") {
         result = ("Rock crushes scissors, you win!")
         console.log(result)
-        humanScore ++
-        return result
+        humanScore++
+        resultDisplay.textContent = result
     } else if (humanChoice === "scissors" && computerChoice === "rock") {
         result = ("Rock crushes scissors, you lose!")
         console.log(result)
-        computerScore ++
-        return result
+        computerScore++
+        resultDisplay.textContent = result
     }
     return [computerScore, humanScore]
+}
+
+function playRound(e) {
+    let target = e.target.className
+    let computerChoice = getComputerChoice()
+    let humanChoice = getHumanChoice(target)
+    updateDisplay(humanChoice, computerChoice)
+    console.log(computerChoice)
+    console.log(humanChoice)
 }
 
 function game() {
@@ -69,10 +79,10 @@ function game() {
     playRound()
     console.log(humanScore, computerScore)
     if (humanScore === 5) {
-        alert("Congradulations, you won!!!!!!!!!!!!!!!")        
-    }   else if (computerScore === 5) {
+        alert("Congradulations, you won!!!!!!!!!!!!!!!")
+    } else if (computerScore === 5) {
         alert("You lost, would you like to play again?")
     }
 }
 
-game()
+choices.addEventListener("click", playRound)
